e429aed1e17fb1468e75106915527740a36bbfe4
dtinth on MBP M1 <dtinth@spacet.me>
2024-12-03 03:42:45 +0700

    implement the Controller/Renderer pattern

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,15 +1,32 @@
+import { useStore } from "@nanostores/react";
+import { atom } from "nanostores";
 import { useState } from "react";
 
-export const Counter = () => {
-  // As a React component grows in scope, it might have 3 main parts.
-  // (1) State management and preparing all the data needed for rendering.
-  const [number, setNumber] = useState(0);
-  const increment = () => setNumber((count) => count + 1);
-  const decrement = () => setNumber((count) => count - 1);
+// A controller class holds the state of the component
+// in a framework-agnostic way using Nano Stores.
+class CounterControler {
+  $count = atom(0);
+}
 
-  // (2) The top-level rendering function.
-  //     It is declared here so that lower-level functions can be defined below it.
-  const render = () => {
+// A renderer class encapsulates the complex logic of rendering a component.
+// It is similar to a React class component in that it has a `render` method,
+// but it is completely stateless and does not inherit from `React.Component`.
+// In each render, it is used once and then discarded.
+class CounterRenderer {
+  // It has 3 parts:
+  // (1) The constructor that takes in the props,
+  //     allowing it to be later accessed in the later parts.
+  constructor(
+    private props: {
+      number: number;
+      decrement: () => void;
+      increment: () => void;
+    }
+  ) {}
+
+  // (2) The top-level `render` method.
+  //     This allows the lower-level methods to be declared below it.
+  render() {
     return (
       <div
         style={{
@@ -19,24 +36,33 @@ export const Counter = () => {
           textAlign: "center",
         }}
       >
-        {renderDecrementButton()}
-        {renderNumber()}
-        {renderIncrementButton()}
+        {this.renderDecrementButton()}
+        {this.renderNumber()}
+        {this.renderIncrementButton()}
       </div>
     );
-  };
+  }
 
-  // (3) The lower-level functions that render each part of the component.
-  const renderDecrementButton = () => {
-    return <button onClick={decrement}>-</button>;
-  };
-  const renderNumber = () => {
-    return <span style={{ flexBasis: "4rem" }}>{number}</span>;
-  };
-  const renderIncrementButton = () => {
-    return <button onClick={increment}>+</button>;
-  };
+  // (3) The lower-level private methods that render each part of the component.
+  private renderDecrementButton() {
+    return <button onClick={this.props.decrement}>-</button>;
+  }
+  private renderNumber() {
+    return <span style={{ flexBasis: "4rem" }}>{this.props.number}</span>;
+  }
+  private renderIncrementButton() {
+    return <button onClick={this.props.increment}>+</button>;
+  }
+}
 
-  // Finally, the top-level rendering function is called to render the component.
-  return render();
+// The main component ties the controller and renderer together.
+export const Counter = () => {
+  const [controller] = useState(() => new CounterControler());
+  const { $count } = controller;
+  const count = useStore($count);
+  return new CounterRenderer({
+    number: count,
+    decrement: () => $count.set(count - 1),
+    increment: () => $count.set(count + 1),
+  }).render();
 };
